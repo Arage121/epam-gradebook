565241120f3be969b8d64ac7133b4125f24056d8
Arage121 <rajayush746@gmail.com>
2023-02-17 19:40:18 +0530

    Added javascript task-1

---
+++ COE1_HW3/homework/js/task1.js
@@ -0,0 +1,21 @@
+// Your code goes here
+function profit(){
+
+    // initial amount
+    let a = parseInt(prompt('Enter the initial amount'))
+    // no. of years
+    let b = parseInt(prompt('Enter no. of years'))
+    //percentage
+    let per = parseInt(prompt('Enter the percentage'))
+    if(isNaN(a) || isNaN(b) || isNaN(per) || a < 1000 || b < 1 || per > 100) {
+        alert('Invalid input data');
+    } else{
+        //formula to print the ans
+        let amount = a*Math.pow(1+per/100, b)
+        let ans1 = amount.toFixed(2)
+        let ans2 = (Number(ans1)-a).toFixed(2)
+        document.getElementById('val').innerHTML = 'Initial amount: '+ a + '<br>'+
+        'Number of years: '+b+'<br>'+'Percentage of year: '+per;
+        document.getElementById('ans').innerHTML = 'Total profit: '+ ans2 +'<br>'+'Total amount: '+ ans1;
+    }
+}
